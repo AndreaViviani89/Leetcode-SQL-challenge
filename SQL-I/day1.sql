9fc5918365a197039fa23bcc1910bce1fef7802d
Andrea Viviani <andrea.viviani89@gmail.com>
2023-01-12 17:00:22 +0100

    Day1 SQL challenge - Done

--- SQL-I/day1.sql
+++ SQL-I/day1.sql
@@ -41,3 +41,70 @@ FROM
 WHERE
     population >= 25000000
 ;
+
+
+
+'second exercise
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| product_id  | int     |
+| low_fats    | enum    |
+| recyclable  | enum    |
++-------------+---------+
+product_id is the primary key for this table.
+low_fats is an ENUM of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
+recyclable is an ENUM of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
+
+Write an SQL query to find the ids of products that are both low fat and recyclable.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+'
+
+
+SELECT
+    product_id
+FROM
+    products
+WHERE low_fats = 'Y' AND recyclable = 'Y'
+
+
+'Another way'
+
+SELECT product_id  
+from products 
+where 'N' not in (low_fats,recyclable)
+
+
+'Third exercise
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+
+
+Write an SQL query to report the names of the customer that are not referred by the customer with id = 2.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+'
+
+SELECT name FROM customer
+WHERE referee_id <> 2 OR referee_id IS NULL;
+
+'Alternative'
+
+SELECT name FROM customer
+WHERE referee_id != 2 OR referee_id IS NULL;
