362270e5e7c10ebee9a4a578e51c690b105a0f82
Andrea Viviani <andrea.viviani89@gmail.com>
2023-01-12 17:09:47 +0100

    Day1 SQL challenge - Done + added alternative solutions

--- SQL-I/day1.sql
+++ SQL-I/day1.sql
@@ -108,3 +108,50 @@ WHERE referee_id <> 2 OR referee_id IS NULL;
 
 SELECT name FROM customer
 WHERE referee_id != 2 OR referee_id IS NULL;
+
+
+
+'
+Fourth exercise
+
+
+Table: Customers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID and name of a customer.
+
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key column for this table.
+customerId is a foreign key of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+
+Write an SQL query to report all customers who never order anything.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+'
+
+select
+    Name as Customers
+from
+    Customers
+where
+    Id
+not in(
+    select CustomerId from Orders
+);
