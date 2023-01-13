0f18aedbf45c8ed66a80cd4cec49698b7ba0d370
Andrea Viviani <andrea.viviani89@gmail.com>
2023-01-13 16:05:14 +0100

    Day2 SQL challenge - DONE

---
+++ SQL-I/day2.sql
@@ -0,0 +1,89 @@
+'
+First exercise
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the employee ID, employee name, and salary.
+
+Write an SQL query to calculate the bonus of each employee. The bonus of an employee is 100% of their salary if the ID of the employee is an odd number and the employee name does not start with the character 'M'. The bonus of an employee is 0 otherwise.
+
+Return the result table ordered by employee_id.
+
+The query result format is in the following example.
+
+'
+
+SELECT employee_id,
+    CASE WHEN employee_id%2 <> 0 AND name not like 'M%' THEN salary 
+    ELSE 0
+        END as bonus
+            FROM Employees 
+                ORDER BY employee_id;
+
+
+
+'
+Second exercise
+
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| id          | int      |
+| name        | varchar  |
+| sex         | ENUM     |
+| salary      | int      |
++-------------+----------+
+id is the primary key for this table.
+The sex column is ENUM value of type ('m', 'f').
+The table contains information about an employee.
+
+
+Write an SQL query to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) with a single update statement and no intermediate temporary tables.
+
+Note that you must write a single update statement, do not write any select statement for this problem.
+
+The query result format is in the following example.
+
+'
+
+
+UPDATE salary
+    SET sex  = (CASE WHEN sex = 'm' 
+        THEN  'f' 
+        ELSE 'm' 
+        END)
+
+
+
+
+'
+Exercise 3
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+
+ 
+
+Write an SQL query to delete all the duplicate emails, keeping only one unique email with the smallest id. Note that you are supposed to write a DELETE statement and not a SELECT one.
+
+After running your script, the answer shown is the Person table. The driver will first compile and run your piece of code and then show the Person table. The final order of the Person table does not matter.
+
+The query result format is in the following example.
+'
+
+DELETE tb1
+FROM Person as tb1, Person as tb2
+WHERE tb1.Email = tb2.Email AND tb1.Id > tb2.Id;
+
